fb3afe7737f902fd95afe8f047b1752648f56119
gitahi <gitahi14043@student.mmarau.ac.ke>
2022-07-03 20:55:33 +0300

    addition of copyTemplateFiles() method

--- commands/forge.js
+++ commands/forge.js
@@ -1,8 +1,32 @@
+const fse = require('fs-extra');
+const chalk = require('chalk');
 const { promptForMissingOptions } = require('../lib/prompts');
+
+async function copyTemplateFiles(options) {
+  try {
+    await fse.copy(options.templateDirectory, options.targetDirectory);
+  } catch (err) {
+    console.log(chalk.red("=========================================="));
+    console.log(chalk.red.bold("An unexpected error occurred while trying to copy template files 👇"));
+    console.error(err);
+    console.log(chalk.red("=========================================="));
+  }
+}
 
 async function forgeProject(options) {
   options = await promptForMissingOptions(options);
   console.log('from forge..\n', options);
+
+  try {
+    await fse.access(options.templateDirectory, fse.constants.R_OK);
+    console.log(chalk.green.bold('Read Access to the template Directory OK!'));
+  } catch (err) {
+    console.log(chalk.red("=========================================="));
+    console.log(chalk.red.bold("An unexpected error occurred while accessing the template directory 👇"));
+    console.error(err);
+    console.log(chalk.red("=========================================="));
+    process.exit(1);
+  }
 }
 
 module.exports = {
